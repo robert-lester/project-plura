1afc4c5703d4f79dafa0abee3c4203083e105cd6
Robert Lester <robertlester@roberts-mbp.lan>
2024-06-21 16:52:07 -0400

    Set up before Auth and after Auth middlerware rules for redircting users

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,70 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { 
+  ClerkMiddlewareAuth,
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
+import { NextResponse, NextRequest } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/agency/sign-in(.*)', '/agency/sign-up(.*)', '/site', '/api/uploadthing']);
+const isPublicRoute = createRouteMatcher([
+  '/agency/sign-in(.*)',
+  '/agency/sign-up(.*)',
+  '/site',
+  '/api/uploadthing'
+]);
+
+const afterAuth = async (auth: ClerkMiddlewareAuth, req: NextRequest) => {
+  // Handle afterAuth logic here
+  //rewrite for domains
+  const url = req.nextUrl
+  const searchParams = url.searchParams.toString()
+  let hostname = req.headers
+
+  const pathWithSearchParams = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ''
+  }`
+
+  //if subdomain exists
+  const customSubDomain = hostname
+    .get('host')
+    ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+    .filter(Boolean)[0]
+
+  if (customSubDomain) {
+    return NextResponse.rewrite(
+      new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+    )
+  }
+
+  if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
+    return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
+  }
+
+  if (
+    url.pathname === '/' ||
+    (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+  ) {
+    return NextResponse.rewrite(new URL('/site', req.url))
+  }
+
+  if (
+    url.pathname.startsWith('/agency') ||
+    url.pathname.startsWith('/subaccount')
+  ) {
+    return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
+  }
+};
 
 export default clerkMiddleware((auth, request) =>{
-  if(!isPublicRoute(request)){
+  // Handle beforeAuth logic here
+  const { userId } = auth();
+  console.log('beforeAuth logic', userId);
+
+  if (!isPublicRoute(request)) {
     auth().protect();
   }
+
+  // Call afterAuth function
+  return afterAuth(auth, request);
 });
 
 export const config = {
