d29072bf792add4d5e9dee814de89605a03c0594
Robert Lester <robertlester@roberts-mbp.lan>
2024-06-26 13:26:29 -0400

    Added create agency page

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,6 +1,6 @@
 import { ModeToggle } from '@/components/global/mode-toggle'
 import { UserButton } from '@clerk/nextjs'
-import { User } from '@clerk/nextjs/server'
+import { currentUser, User } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -9,7 +9,9 @@ type Props = {
   user?: null | User
 }
 
-const Navigation = ({ user }: Props) => {
+const Navigation = async ({  }: Props) => {
+  const user = await currentUser();
+
   return (
     <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
       <aside className="flex items-center gap-2">
@@ -30,12 +32,14 @@ const Navigation = ({ user }: Props) => {
         </ul>
       </nav>
       <aside className="flex gap-2 items-center">
-        <Link
-          href={'/agency'}
-          className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
-        >
-          Login
-        </Link>
+        { !user && 
+          <Link
+            href={'/agency'}
+            className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
+          >
+            Login
+          </Link>
+        }
         <UserButton />
         <ModeToggle />
       </aside>
